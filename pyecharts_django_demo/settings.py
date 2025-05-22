186e0e4e997118c995280fdfbb6ffb7aeb748de4
wzp100 <wzp100@outlook.com>
2025-05-22 09:36:51 +0800

    使用env

--- pyecharts_django_demo/settings.py
+++ pyecharts_django_demo/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = Path(BASE_DIR, "static_collected")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
